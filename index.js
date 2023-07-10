8b3449d55dbc38487329b240120b37d29a651f5d
Yazan Unis <yazanunis89@gmail.com>
2023-07-10 14:42:32 +0300

    Fifth commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors())
 
-app.use('/posts', postRouter) 
+app.use('/posts', postRouter)
 
 app.get('/', (req, res) => res.send("Welcome To Memories API"))
 
