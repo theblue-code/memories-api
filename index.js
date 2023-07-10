e26e407c35add4d90df66c27a28e1913a1af9bd4
Yazan Unis <yazanunis89@gmail.com>
2023-07-10 15:06:32 +0300

    final commit

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import dotenv from 'dotenv'
-import postRouter from './routes/posts.js'
+
+import PostMessage from './models/postMessage.js'
 
 const app = express()
 dotenv.config()
@@ -12,11 +13,41 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors())
 
-app.use('/posts', postRouter)
-
 app.get('/', (req, res) => res.send("Welcome To Memories API"))
 
-const PORT = process.env.PORT
+app.get('/posts', async (req, res) => {
+    try {
+        const postMessages = await PostMessage.find()
+
+        res.status(200).json(postMessages)
+    } catch (error) {
+        res.status(404).json({ message: error.message })
+    }
+})
+
+app.post('/posts', async (req, res) => {
+    const post = req.body
+
+    const newPost = new PostMessage(post)
+    try {
+        await newPost.save()
+        res.status(201).json(newPost)
+    } catch (error) {
+        res.status(409).json({ message: error.message })
+    }
+})
+
+app.delete('/posts/:id', async (req, res) => {
+    const { id } = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`)
+
+    await PostMessage.findByIdAndDelete(id)
+
+    res.json({ message: "Post deleted successfully" })
+})
+
+const PORT = process.env.PORT || 5000
 
 mongoose.connect(process.env.CONNECTION_URL)
     .then(app.listen(PORT, () => console.log(`Server is running on ${PORT}`)))
